3a445b61d1795a87075cea5eecc021359a207d4b
menison-notebook <menison7@mail.ru>
2023-09-19 13:23:25 +0300

    step 7

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,39 +2,43 @@ import prompt
 import random
 
 
-def welcome_user():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    return name
+def main():
+    def welcome_user():
+        print("Welcome to the Brain Games!")
+        name = prompt.string('May I have your name? ')
+        return name
 
-def even():
-    l = ['yes','no']
-    print('''Answer "yes" if the number is even, otherwise answer "no".''')
-    n = random.randint(1,100)
-    print("Question:",n)
-    answer = prompt.string('Your answer: ')
-    if n%2 == l.index(answer.lower()):
-        print('Correct!')
-        return 1
-    else:
-        print(f''''{answer}' is wrong answer ;(. ''',end = " ")
-        l.remove(answer)
-        print(f'''Correct answer was '{l[0]}'.''')
-        print(f"Let's try again, {name}!")
-        return 0
+    def even():
+        l = ['yes','no']
+        print('''Answer "yes" if the number is even, otherwise answer "no".''')
+        n = random.randint(1,100)
+        print("Question:",n)
+        answer = prompt.string('Your answer: ')
+        if n%2 == l.index(answer.lower()):
+            print('Correct!')
+            return 1
+        else:
+            print(f''''{answer}' is wrong answer ;(. ''',end = " ")
+            l.remove(answer)
+            print(f'''Correct answer was '{l[0]}'.''')
+            print(f"Let's try again, {name}!")
+            return 0
 
 
-name = welcome_user()
-print(f"Hello, {name}")
+    name = welcome_user()
+    print(f"Hello, {name}")
 
-n = 0
-for i in range(3):
-    if even() == 0:
-        break
-    else:
-        n+=1
+    n = 0
+    for i in range(3):
+        if even() == 0:
+            break
+        else:
+            n+=1
 
-if n == 3:
-    print(f"Congratulations, {name}!")
+    if n == 3:
+        print(f"Congratulations, {name}!")
+
+if __name__ == '__main__':
+    main()
 
 
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,41 @@
+import prompt
+import random
+
+def main():
+    def welcome_user():
+        print("Welcome to the Brain Games!")
+        name = prompt.string('May I have your name? ')
+        return name
+
+    def gcd():
+        a = random.randint(1,100)
+        b = random.randint(1,100)
+        nod = 0
+        print(f"Question: {min(a,b)} {max(a,b)}")
+
+        for i in range(1,max(a,b)):
+            if a % i == 0 and b % i == 0:
+                nod = i
+        user_answer = prompt.string('Your answer: ')
+        if nod == int(user_answer):
+            return 1
+        else:
+            return 0
+
+
+
+    name = welcome_user()
+    print(f"Hello, {name}")
+    print("Find the greatest common divisor of given numbers.")
+    n = 0
+    for i in range(3):
+        if gcd() == 0:
+            break
+        else:
+            n+=1
+
+    if n == 3:
+        print(f"Congratulations, {name}!")
+        
+if __name__ == '__main__':
+    main()
