1ef36d8a302fa13bc28df382cdaa705ef0c11a60
menison-notebook <menison7@mail.ru>
2023-09-19 17:35:32 +0300

    step 8

--- Makefile
+++ Makefile
@@ -16,11 +16,6 @@ package-install:
 
 make lint:
 	poetry run flake8 brain_games
-brain-even:
-	poetry run brain-even
-	
-brain-calc:
-	poetry run brain-calc
 
-brain-gcd:
-	poetry run brain-gcd
+
+    
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -7,32 +7,31 @@ def main():
         name = prompt.string('May I have your name? ')
         return name
 
-    def gcd():
-        a = random.randint(1,100)
-        b = random.randint(1,100)
-        nod = 0
-        print(f"Question: {min(a,b)} {max(a,b)}")
-
-        for i in range(1,max(a,b)):
-            if a % i == 0 and b % i == 0:
-                nod = i
+    def progression():
+        shag = random.randint(1,6)
+        l = [random.randint(0,10),]
+        for i in range(10):
+            l.append(l[i]+shag)
+        found_element = l[random.randint(0,10)]
+        l[l.index(found_element)] ='..'
+        print(*l)
         user_answer = prompt.string('Your answer: ')
-        if nod == int(user_answer):
+        if found_element == int(user_answer):
             print('Correct!')
             return 1
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{nod}'.")
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{found_element}'.")
             print(f"Let's try again, {name}!")
             return 0
 
 
-
+    
     name = welcome_user()
     print(f"Hello, {name}")
-    print("Find the greatest common divisor of given numbers.")
+    print("What number is missing in the progression?")
     n = 0
     for i in range(3):
-        if gcd() == 0:
+        if progression() == 0:
             break
         else:
             n+=1
