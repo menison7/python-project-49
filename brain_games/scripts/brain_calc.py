d9b3a9ab4d8c7215e6bf6d226a28f782f97abf16
menison-notebook <menison7@mail.ru>
2023-09-29 12:12:49 +0300

    fixsed 4

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -16,43 +16,18 @@ def calc():
     for i in range(3):
         a = random.randint(1, 100)
         b = random.randint(1, 100)
-        z = random.randint(1, 4)
-        if z == 1:
-            r = a + b
-            print(f"Question: {a} + {b}")
-            user_answer = prompt.string('Your answer: ')
-            if r == int(user_answer):
-                print("Correct!")
-
-            else:
-                print(f''''{user_answer}' is wrong answer ;(.''', end='')
-                print(f''' Correct answer was '{r}'.''')
-                print(f"Let's try again, {name}!")
-                break
-        elif z == 2:
-            r = a - b
-            print(f"Question: {a} - {b}")
-            user_answer = prompt.string('Your answer: ')
-            if r == int(user_answer):
-                print("Correct!")
-
-            else:
-                print(f''''{user_answer}' is wrong answer ;(.''', end='')
-                print(f''' Correct answer was '{r}'.''')
-                print(f"Let's try again, {name}!")
-                break
+        z = random.randint(0, 2)
+        z_l = ['-', '+', '*']
+        rez = eval(str(a) + z_l[z] + str(b))
+        print(f"Question: {a} {z_l[z]} {b}")
+        user_answer = prompt.string('Your answer: ')
+        if int(user_answer) == rez:
+            print("Correct!")
         else:
-            r = a * b
-            print(f"Question: {a} * {b}")
-            user_answer = prompt.string('Your answer: ')
-            if r == int(user_answer):
-                print("Correct!")
-
-            else:
-                print(f''''{user_answer}' is wrong answer ;(.''', end='')
-                print(f''' Correct answer was '{r}'.''')
-                print(f"Let's try again, {name}!")
-                break
+            print(f''''{user_answer}' is wrong answer ;(.''', end='')
+            print(f''' Correct answer was '{rez}'.''')
+            print(f"Let's try again, {name}!")
+            break
         ch += 1
         if ch == 3:
             print(f"Congratulations, {name}!")
